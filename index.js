b6a149116df96d90a65730e25084767b8a0ba86b
Michael <michael.d.lange88@gmail.com>
2022-10-19 11:33:36 -0700

    commit tree 46737dc4a17579e196a9839ea193edf4a96972c9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainDel = document.getElementById("main");
+mainDel.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Michael is the champion"
